d56dd4623cc28181b218b16ec562ce79141abda5
Aintripin <yelisey_toyota@mail.ru>
2022-03-10 20:09:18 +0300

    brushed up the App.js file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const[count, setCount] = useState(0);
   return (
     <div className="app">
-      <h1>{count}</h1>
+      <h1 className = {count > 0 ? "positive" : count < 0 ? "negative" : null}>{count}</h1>
       <div className = "button-wrapper">
         <button onClick = {() => setCount(count-1)}>-</button>
         <button onClick = {() => setCount(count+1)}>+</button>
